db082ba1cfc8d81d3a09f7d635dd74672fbbcd4c
Olga Kitova <olga.vizgalova@bk.ru>
2021-02-09 18:14:33 +0500

    feat: implemented carbon-dating, count-cats, dream-team, hanoi-tower,  transform-array and what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false;
+  }
+members = members.map((item) => {
+  if(typeof(item) == 'string') {
+     return item.replace(/\s+/g,'').toUpperCase();
+  }
+});
+members.sort();
+let twoArray = [];
+for(let i = 0; i < members.length; i++) {
+if(typeof(members[i]) == 'string') {
+  twoArray.push(members[i][0]);
+}
+}
+return twoArray.join('');
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) {
+     throw new Error();
+  }
+  let arr2 = [];
+  const intermediate = [];
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] == '--discard-next') {
+      intermediate.push(++i);
+    } else if (arr[i] == '--discard-prev') {
+      if (arr2.length > 0 && !intermediate.some((x) => x === i - 1)) {
+        arr2 = arr2.slice(0, arr2.length - 1);
+        intermediate.push(i - 1);
+      }
+    } else if (arr[i] == '--double-next') {
+      if (++i < arr.length) {
+       arr2.push(arr[i]);
+        arr2.push(arr[i]);
+      }
+    } else if (arr[i] == '--double-prev') {
+      if (i > 0 && !intermediate.some((x) => x === i - 1))
+        arr2.push(arr[i - 1]);
+    } else {
+      arr2.push(arr[i]);
+    }
+  }
+  return arr2;
 };

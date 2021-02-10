e03b0764a8f5f06ba84379781d355438c5bc9e8b
Olga Kitova <olga.vizgalova@bk.ru>
2021-02-10 10:54:31 +0500

    feat: realise extended-repeater and vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let adition = "";
+  if (options && options.addition !== undefined) {
+    adition = Array(options.additionRepeatTimes || 1).fill(String(options.addition)).join(options.additionSeparator || "|");
+  }
+  let collectStr = Array(options.repeatTimes || 1).fill(String(str + adition)).join(options.separator || "+");
+  return collectStr;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value=true){
+        if (value === undefined) {
+          throw new Error();
+        }
+        this.value = value;
+        this.alphaArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+      }
+  encrypt(msg, key) {
+     if (msg === undefined || key === undefined) {
+       throw new Error();
+     }
+        let keyArray = key.toUpperCase().split('');
+        let keyPosition = 0;
+        let retValue = msg.toUpperCase().split('').map( x => {
+            if (this.alphaArray.indexOf(x) >= 0){
+                let shift = this.alphaArray.indexOf(keyArray[keyPosition]);
+                let newIdx = (shift + this.alphaArray.indexOf(x)) % this.alphaArray.length;
+                keyPosition = ++keyPosition % keyArray.length;
+                return this.alphaArray[newIdx];
+            }
+            return x;
+        } );
+        return this.value ? retValue.join('') : retValue.reverse().join('');
+  }   
+  decrypt(msg, key) {
+      if (msg === undefined || key === undefined) {
+        throw new Error();
+      }
+        let keyArray = key.toUpperCase().split('');
+        let keyPosition = 0;
+        let retValue = msg.toUpperCase().split('').map( x => {
+            if (this.alphaArray.indexOf(x) >= 0){
+                let shift = this.alphaArray.indexOf(keyArray[keyPosition]);
+                let newIdx = (this.alphaArray.indexOf(x) - shift);
+                if (newIdx < 0) newIdx += this.alphaArray.length;
+                keyPosition = ++keyPosition % keyArray.length;
+                return this.alphaArray[newIdx];
+            }
+            return x;
+        });
+        return this.value ? retValue.join('') : retValue.reverse().join('');
   }
 }
 
